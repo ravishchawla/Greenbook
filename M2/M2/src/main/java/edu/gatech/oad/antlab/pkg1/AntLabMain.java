f13a35ad60b7c03516cecf2b5d0c13750aa2f2f6
Yogikrupa <Yogikrupa@Yogikrupa-HP>
2014-01-23 21:57:51 -0500

    2nd update antlabmain

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Devang Patel");
-                  toPrint += p5.toString("902958875");
+                  toPrint += p5.toString("dpatel304");
 		  
         System.out.println(toPrint);
         
