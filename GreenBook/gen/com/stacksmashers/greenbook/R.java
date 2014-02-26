875fe4dd5c8a26869109d1d85b187c5f9f2877a4
Ravish Chawla <rchawla8@github.com>
2014-02-26 00:22:09 -0500

    Added a whole bunch of functionality. Added color coding, sorting, design tweaks, etc.

--- GreenBook/gen/com/stacksmashers/greenbook/R.java
+++ GreenBook/gen/com/stacksmashers/greenbook/R.java
@@ -25,23 +25,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int greenbooklauncher=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int content_new=0x7f020000;
+        public static final int content_sort=0x7f020001;
+        public static final int greenbooklauncher=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f090016;
-        public static final int account_display_balance=0x7f090018;
-        public static final int account_display_bank=0x7f090017;
+        public static final int RelativeLayout1=0x7f090018;
+        public static final int account_display_balance=0x7f09001a;
+        public static final int account_display_bank=0x7f090019;
         public static final int account_display_name=0x7f090012;
-        public static final int accounts_add=0x7f09001a;
+        public static final int accounts_add=0x7f09001c;
+        public static final int accounts_balance_sort=0x7f090020;
+        public static final int accounts_bank_sort=0x7f090021;
+        public static final int accounts_color_sort=0x7f09001e;
         public static final int accounts_dialog=0x7f09000f;
         public static final int accounts_dialog_balance=0x7f090013;
         public static final int accounts_dialog_bank=0x7f090011;
         public static final int accounts_dialog_color=0x7f090014;
         public static final int accounts_dialog_name=0x7f090010;
         public static final int accounts_display_custom=0x7f090015;
+        public static final int accounts_interst=0x7f090016;
+        public static final int accounts_interst_name=0x7f090017;
         public static final int accounts_list=0x7f090000;
-        public static final int action_settings=0x7f090019;
+        public static final int accounts_name_sort=0x7f09001f;
+        public static final int accounts_sort=0x7f09001d;
+        public static final int accounts_spinner=0x7f090022;
+        public static final int action_settings=0x7f09001b;
         public static final int checkpasswordCheck=0x7f09000e;
         public static final int emailCheck=0x7f09000c;
         public static final int loginButton=0x7f090005;
--- GreenBook/src/com/stacksmashers/greenbook/AccountsActivity.java
+++ GreenBook/src/com/stacksmashers/greenbook/AccountsActivity.java
@@ -1,11 +1,11 @@
 package com.stacksmashers.greenbook;
 
 import android.app.AlertDialog;
+import android.content.ClipData.Item;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,18 +16,23 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-   /**
-    * 
-    * this class calls all account activity within the application 
-    *it is registry of user's account. 
-    *
-    */
-public class AccountsActivity extends BaseActivity 
+/**
+ * 
+ * this class calls all account activity within the application it is registry
+ * of user's account.
+ * 
+ */
+public class AccountsActivity extends BaseActivity
 {
 	private View dialogView;
 	private ListView list;
@@ -37,26 +42,27 @@ public class AccountsActivity extends BaseActivity
 	private Spinner choose_bank;
 	private Spinner choose_color;
 	private SimpleCursorAdapter adap;
+	private CheckBox check_interst;
+	private TextView add_interst;
 	private String accountsUser, userType;
 	private int userID;
-	
-	
+	private static boolean redo = false;
 
 	public AccountsActivity()
 	{
 		// TODO Auto-generated constructor stub
 	}
 
-    /**
+	/**
 	 * @param args
-	 * main method that calls other method
-     */
+	 *            main method that calls other method
+	 */
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 
 	}
-      
+
 	@Override
 	/**called this method to start the activity.  
 	 * Maintain the activity and application.
@@ -67,90 +73,139 @@ public class AccountsActivity extends BaseActivity
 	{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.activity_accounts);     // calling setcontentview from res 
-		
+
+		setContentView(R.layout.activity_accounts); // calling setcontentview
+													// from res
+
 		Bundle extras = getIntent().getExtras();
-		
+
 		userType = extras.getString("Account Type");
 		accountsUser = extras.getString("Account User");
-		
-		list = (ListView)findViewById(R.id.accounts_list);
-		
-				
-		
-		//Toast.makeText(getApplicationContext(), "User: " + accountsUser, Toast.LENGTH_LONG).show();
+
+		list = (ListView) findViewById(R.id.accounts_list);
+
+		// Toast.makeText(getApplicationContext(), "User: " + accountsUser,
+		// Toast.LENGTH_LONG).show();
 		Log.i("TAG", "User: " + accountsUser);
-		
-		Cursor csr = sqldbase.query(DBHelper.USER_TABLE, new String[]{DBHelper.USERS_ID}, DBHelper.USER_NAME + " = '" + accountsUser + "'", null,null,null,null); 
-		
+
+		Cursor csr = sqldbase.query(DBHelper.USER_TABLE,
+				new String[] { DBHelper.USERS_ID }, DBHelper.USER_NAME + " = '"
+						+ accountsUser + "'", null, null, null, null);
+
 		if (csr.getCount() != 0)
 		{
 			csr.moveToFirst();
 			userID = csr.getInt(0);
-			Log.i("User ID: ", ""+userID);	
+			Log.i("User ID: ", "" + userID);
 		}
-		
-		
-		
-		
-		updateData();
-	
-		
+
+		updateData(DBHelper.ACCOUNT_ID);
+
 	}
-	
-	
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-	
+
 		getMenuInflater().inflate(R.menu.menu_accounts, menu);
-		
+
 		// TODO Auto-generated method stub
 		return super.onCreateOptionsMenu(menu);
 	}
-	
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
-	
-		if(item.getItemId() == R.id.accounts_add)
+
+		if (item.getItemId() == R.id.accounts_add)
 		{
 			addAccount();
-			
+
 		}
-		
+
+		else if (item.getItemId() == R.id.accounts_name_sort)
+		{
+
+			updateData(DBHelper.ACCOUNT_NAME);
+
+		}
+
+		else if (item.getItemId() == R.id.accounts_balance_sort)
+		{
+
+			updateData(DBHelper.ACCOUNT_BALANCE);
+		}
+
+		else if (item.getItemId() == R.id.accounts_color_sort)
+		{
+			updateData(DBHelper.ACCOUNT_COLOR);
+		}
+
+		else if (item.getItemId() == R.id.accounts_bank_sort)
+		{
+			updateData(DBHelper.ACCOUNT_BANK);
+		}
+
 		// TODO Auto-generated method stub
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
-	
-	
+
 	public void addAccount()
 	{
-dialogView = getLayoutInflater().inflate(R.layout.dialog_accounts, null);;
+
+		dialogView = getLayoutInflater()
+				.inflate(R.layout.dialog_accounts, null);
+
+		add_name = (TextView) dialogView
+				.findViewById(R.id.accounts_dialog_name);
+		add_balance = (TextView) dialogView
+				.findViewById(R.id.accounts_dialog_balance);
+		add_custom = (AutoCompleteTextView) dialogView
+				.findViewById(R.id.accounts_display_custom);
+		choose_bank = (Spinner) dialogView
+				.findViewById(R.id.accounts_dialog_bank);
+		choose_color = (Spinner) dialogView
+				.findViewById(R.id.accounts_dialog_color);
+
+		check_interst = (CheckBox) dialogView
+				.findViewById(R.id.accounts_interst);
 		
-		add_name = (TextView)dialogView.findViewById(R.id.accounts_dialog_name);
-		add_balance = (TextView)dialogView.findViewById(R.id.accounts_dialog_balance);
-		add_custom = (AutoCompleteTextView)dialogView.findViewById(R.id.accounts_display_custom);
-		choose_bank = (Spinner)dialogView.findViewById(R.id.accounts_dialog_bank);
-		choose_color = (Spinner)dialogView.findViewById(R.id.accounts_dialog_color);
+		add_interst = (TextView) dialogView
+				.findViewById(R.id.accounts_interst_name);
 		
+		add_interst.setVisibility(View.GONE);
+		
+		check_interst.setOnCheckedChangeListener(new OnCheckedChangeListener()
+		{
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+			{
+				// TODO Auto-generated method stub
+				
+				if(!isChecked)
+				{
+					add_interst.setVisibility(View.GONE);
+				}
+				else
+					add_interst.setVisibility(View.VISIBLE);
+				
+			}
+		});
+		
+		
+
 		add_custom.setVisibility(View.GONE);
 
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Banks, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.Banks, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		choose_bank.setAdapter(adapter);
-				
-		adapter = ArrayAdapter.createFromResource(this, R.array.Colors, android.R.layout.simple_spinner_item);
+
+		adapter = ArrayAdapter.createFromResource(this, R.array.Colors,
+				android.R.layout.simple_spinner_item);
 		choose_color.setAdapter(adapter);
-		
-		
-		
+
 		choose_bank.setOnItemSelectedListener(new OnItemSelectedListener()
 		{
 
@@ -159,167 +214,209 @@ dialogView = getLayoutInflater().inflate(R.layout.dialog_accounts, null);;
 					int pos, long id)
 			{
 				// TODO Auto-generated method stub
-				
 
-				if(pos == (choose_bank.getCount() -1))
+				if (pos == (choose_bank.getCount() - 1))
 				{
 					add_custom.setVisibility(View.VISIBLE);
 				}
-			
+
 				else
 					add_custom.setVisibility(View.GONE);
-				
+
 				Log.i("Selected id: ", choose_bank.getCount() + " - " + pos);
-			
-				
+
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0)
 			{
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 
-		
-		
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		
-			builder.setView(dialogView).setPositiveButton("Add", new OnClickListener()
-			{
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which)
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		final AlertDialog ArtDialog = builder.setView(dialogView)
+				.setPositiveButton("Add", new OnClickListener()
 				{
-					// TODO Auto-generated method stub
-					
-					//Toast.makeText(getApplicationContext(), choose_bank.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
-					
-					
-					
-					
-					ContentValues caeser = new ContentValues();
-					caeser.put(DBHelper.ACCOUNT_NAME, add_name.getText().toString());
-					caeser.put(DBHelper.ACCOUNT_BALANCE, add_balance.getText().toString());
-					caeser.put(DBHelper.ACCOUNT_BANK, choose_bank.getSelectedItem().toString());
-					caeser.put(DBHelper.ACCOUNT_COLOR, choose_color.getSelectedItem().toString());
-					caeser.put(DBHelper.ACCOUNT_USER, userID);
-					sqldbase.insert(DBHelper.ACCOUNT_TABLE, null, caeser);
-					updateData();
-					//caeser.put(DBHelper.ACCOUNT, value)
-		
-					
-					
-				}
-			}).setTitle("Enter Details").setNegativeButton("Cancel", new OnClickListener(){
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which)
+
+					@Override
+					public void onClick(DialogInterface dialog, int which)
+					{
+						// TODO Auto-generated method stub
+
+						// Toast.makeText(getApplicationContext(),
+						// choose_bank.getSelectedItem().toString(),
+						// Toast.LENGTH_LONG).show();
+					}
+				}).setTitle("Enter Details")
+				.setNegativeButton("Cancel", new OnClickListener()
 				{
-						
-					
+
+					@Override
+					public void onClick(DialogInterface dialog, int which)
+					{
+
 						dialog.cancel();
-				}
-			}).show();
-			
-			
-		
-			
-			
-			
-			
-			
-	}
-	
-	
-	@SuppressWarnings("deprecation")
-	public void updateData()
-	{
-		
-		
-		
-		String from[] = {DBHelper.ACCOUNT_NAME, DBHelper.ACCOUNT_BANK, DBHelper.ACCOUNT_BALANCE};
-		String query[] = {DBHelper.ACCOUNT_ID, DBHelper.ACCOUNT_NAME, DBHelper.ACCOUNT_BANK, DBHelper.ACCOUNT_BALANCE, DBHelper.ACCOUNT_COLOR};
-		int to[] = {R.id.account_display_name, R.id.account_display_bank, R.id.account_display_balance};
-		
-		
-		
-		
-		final Cursor csr = sqldbase.query(DBHelper.ACCOUNT_TABLE, query, DBHelper.ACCOUNT_USER + " = '" + userID + "'", null, null, null, null);
-		
-		Log.i("TAG", "Cursor Adap = null: " + csr.getCount());
-		
-		if(csr.getCount() != 0)
+					}
+				}).create();
+
+		ArtDialog.setOnShowListener(new DialogInterface.OnShowListener()
 		{
-			csr.moveToFirst();
-			adap = new SimpleCursorAdapter(getBaseContext(), R.layout.listblock_accounts, csr, from, to){
-		
-			
-				@Override
-				public View getView(int position, View convertView, ViewGroup parent){
-					
-					View v = super.getView(position, convertView, parent);
-					
-					String color = csr.getString(4);
-					Log.i("color: " , color);
-					int setColor;
-					if(color.equals("Red"))
-						setColor = getResources().getColor(android.R.color.holo_red_light);
-					else if (color.equals("Green"))
-						setColor = getResources().getColor(android.R.color.holo_green_dark);
-					else if (color.equals("Blue"))
-						setColor = getResources().getColor(android.R.color.holo_blue_light);
-					else if (color.equals("Yellow"))
-						setColor = getResources().getColor(android.R.color.holo_green_light);
-					else if (color.equals("Cyan"))
-						setColor = getResources().getColor(android.R.color.holo_blue_bright);
-					else if (color.equals("Magenta"))
-						setColor = getResources().getColor(android.R.color.holo_purple);
-					else 
-						setColor = getResources().getColor(android.R.color.white);
-					
-					
-					v.setBackgroundColor(setColor);
-					csr.moveToNext();
-					
-					return v;
-					
-					
-				}
-				
-				
-			};
-			
-			
-			
-			
-			list.setAdapter(adap);
-		}
-		
-		
-		
-		
-		
-		
+
+			@Override
+			public void onShow(DialogInterface dialog)
+			{
+				// TODO Auto-generated method stub
+
+				Button plus = ArtDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+
+				plus.setOnClickListener(new View.OnClickListener()
+				{
+
+					@Override
+					public void onClick(View v)
+					{
+						// TODO Auto-generated method stub
+
+						final ContentValues caeser = new ContentValues();
+						caeser.put(DBHelper.ACCOUNT_NAME, add_name.getText()
+								.toString());
+						caeser.put(DBHelper.ACCOUNT_BALANCE, add_balance
+								.getText().toString());
+						caeser.put(DBHelper.ACCOUNT_BANK, choose_bank
+								.getSelectedItem().toString());
+						caeser.put(DBHelper.ACCOUNT_COLOR, choose_color
+								.getSelectedItem().toString());
+						caeser.put(DBHelper.ACCOUNT_USER, userID);
+
+						if(check_interst.isChecked())
+						{
+							caeser.put(DBHelper.ACCOUNT_INTEREST, add_interst.getText().toString());
+						}
+						else
+							caeser.put(DBHelper.ACCOUNT_INTEREST, "-1");
+						
+						
+						
+						Cursor csr = sqldbase.query(DBHelper.ACCOUNT_TABLE,
+								new String[] { DBHelper.ACCOUNT_NAME },
+								DBHelper.ACCOUNT_USER + " = '" + userID
+										+ "' AND " + DBHelper.ACCOUNT_NAME
+										+ " = '"
+										+ add_name.getText().toString() + "'",
+								null, null, null, null);
+
+						if (csr.getCount() != 0)
+						{
+							// if (!redo)
+							// {
+
+							Toast.makeText(
+									getApplicationContext(),
+									"Account Alreaddy Exists\nPlease Rename the Account",
+									Toast.LENGTH_LONG).show();
+							// redo = true;
+							// }
+							// else
+							// {
+							// sqldbase.insert(DBHelper.ACCOUNT_TABLE, null,
+							// caeser);
+							// redo = false;
+
+							// ArtDialog.dismiss();
+							// updateData();
+							// }
+						}
+						else
+						{
+							sqldbase.insert(DBHelper.ACCOUNT_TABLE, null,
+									caeser);
+							ArtDialog.dismiss();
+							updateData(DBHelper.ACCOUNT_ID);
+						}
+						// caeser.put(DBHelper.ACCOUNT, value)
+
+					}
+
+				});
+			}
+		});
+
+		ArtDialog.show();
+
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@SuppressWarnings("deprecation")
+	public void updateData(String Sort)
+	{
+
+		String from[] = { DBHelper.ACCOUNT_NAME, DBHelper.ACCOUNT_BANK,
+				DBHelper.ACCOUNT_BALANCE };
+		String query[] = { DBHelper.ACCOUNT_ID, DBHelper.ACCOUNT_NAME,
+				DBHelper.ACCOUNT_BANK, DBHelper.ACCOUNT_BALANCE,
+				DBHelper.ACCOUNT_COLOR };
+		int to[] = { R.id.account_display_name, R.id.account_display_bank,
+				R.id.account_display_balance };
+
+		final Cursor csr = sqldbase.query(DBHelper.ACCOUNT_TABLE, query,
+				DBHelper.ACCOUNT_USER + " = '" + userID + "'", null, null,
+				null, Sort);
+
+		Log.i("TAG", "Cursor Adap = null: " + csr.getCount());
+
+		if (csr.getCount() != 0)
+		{
+			csr.moveToFirst();
+			adap = new SimpleCursorAdapter(getBaseContext(),
+					R.layout.listblock_accounts, csr, from, to)
+			{
+
+				@Override
+				public View getView(int position, View convertView,
+						ViewGroup parent)
+				{
+
+					View v = super.getView(position, convertView, parent);
+
+					String color = csr.getString(4);
+					Log.i("color: ", color);
+					int setColor;
+					if (color.equals("Red"))
+						setColor = getResources().getColor(
+								android.R.color.holo_red_light);
+					else if (color.equals("Green"))
+						setColor = getResources().getColor(
+								android.R.color.holo_green_dark);
+					else if (color.equals("Blue"))
+						setColor = getResources().getColor(
+								android.R.color.holo_blue_light);
+					else if (color.equals("Yellow"))
+						setColor = getResources().getColor(
+								android.R.color.holo_green_light);
+					else if (color.equals("Cyan"))
+						setColor = getResources().getColor(
+								android.R.color.holo_blue_bright);
+					else if (color.equals("Magenta"))
+						setColor = getResources().getColor(
+								android.R.color.holo_purple);
+					else
+						setColor = getResources().getColor(
+								android.R.color.white);
+
+					v.setBackgroundColor(setColor);
+					csr.moveToNext();
+
+					return v;
+
+				}
+
+			};
+
+			list.setAdapter(adap);
+		}
+
+	}
+
 }
